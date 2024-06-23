1fc5320efeb249ab96c636584a8f0a36380c6bd2
bata26 <batalonilorenzo@gmail.com>
2024-06-23 08:29:09 +0000

    logs

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ mvn -f lettercount-naive clean package
 mvn -f lettercount-opt clean package
 mvn -f lettercount-opt-combiner clean package
 
-files=(IT_1GB.txt IT_3GB.txt)
-versions=(opt opt-combiner naive)
-reducers=(1 7 13 20 26)
+files=(IT_3GB.txt)
+versions=(naive)
+reducers=(13 20 26)
 
 for file in ${files[@]}; do
     for ver in ${versions[@]}; do
@@ -20,7 +20,7 @@ for file in ${files[@]}; do
            {
                 hadoop jar ./lettercount-${ver}/target/lettercount-${ver}-1.0-SNAPSHOT.jar it.unipi.hadoop.LetterCount ${file} results/${ver}/${file}/${num} ${num}
                 echo "waiting for killing container...."
-                sleep 300 # Wait 5 min. to let all containers finish (60sec x 5)
+                sleep 150 # Wait 5 min. to let all containers finish (60sec x 5)
             } >> ${log_file} 2>&1
         done
     done
